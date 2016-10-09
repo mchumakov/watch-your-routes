eb18e7fca3621b28f3b4b99b1663b6accf960252
Maksim Chumakov <mg.chumakov@gmail.com>
2016-10-10 00:57:58 +0300

    a lot of chanegs, including interface check, routing tables check

--- routing.py
+++ routing.py
@@ -8,20 +8,75 @@ import time
 isp1_if = 'ens33'
 isp1_gw_ip = '172.16.0.254'
 isp1_link_type = 'ethernet'
+isp1_addr = '172.16.0.253'
 isp2_if = 'ens37'
 isp2_gw_ip = '172.16.1.254'
 isp2_link_type = 'pppoe'
+isp2_addr = '172.16.1.253'
 testip1 = '94.198.134.60'
 testip2 = '89.169.1.102'
 testip3 = '8.8.8.8'
 cmd = 'fping -I isp2_if testip3 testip1' 
 health_counter = 0
+isp1_failed = False
+isp2_failed = False
 
-ipr = IPRoute() #Get IPRoute object.
+ipr = IPRoute() #Get IPRoute object to access Netlink data.
+#Get installed ip addresses on ISP interfaces to ensure we can bind to them during further tests. 
+def check_if_state(f_isp1_if, f_isp2_if):
+    isp1_if_index = ipr.link_lookup(ifname = f_isp1_if)
+    if len(isp1_if_index) == 0:
+        print "Interface %s not found in the system." % f_isp1_if
+        isp1_failed = True
+    else:
+        isp1_if_data = ipr.get_addr(family=2,index=isp1_if_index[0])
+   
+    isp2_if_index = ipr.link_lookup(ifname = f_isp2_if)
+    if len(isp2_if_index) == 0:
+        print "Interface %s not found in the system." % f_isp2_if
+        isp2_failed = True
+    else:
+        isp2_if_data = ipr.get_addr(family=2,index=isp2_if_index[0])
+   
 
+    if len(isp1_if_data) == 0:
+        #no ip address found on interface marking isp1 interface as failed
+        isp1_failed = True
+    else:
+        isp1_failed = False
+
+    if len(isp2_if_data) == 0:
+       #no ip address found on interface marking isp2 interface as failed
+        isp2_failed = True
+    else:
+        isp2_failed = False
+    
+    return isp1_failed, isp2_failed 
+
+#if_status = check_if_state(isp1_if, isp2_if)
+#print if_status
+#if if_status[0] == False or if_status[1] == False:
+#   exit() 
+        
 #Function to check which default route installed in routing table.
 def check_def_routes():
-    routes = ipr.get_routes(dst_len=0) #Obtain all default routes in routing table main. But we need to check return value to handle situation when there is no any default route installed.
+    routes = ipr.get_routes(dst_len=0, table=254) #Obtain all default routes in routing table main. But we need to check return value to handle situation when there is no any default route installed.
+    routes_isp1 = ipr.get_routes(dst_len=0, table=216) #Obtain route information from table 216 - ISP1
+    routes_isp2 = ipr.get_routes(dst_len=0, table=217) #obtain route information from table 217 - ISP2
+#then we check if there corresponding deafult routes installes in each of the ISP tables
+#if there is no default route installes we insert it, if there is incorrect one we change it to correct one
+    if len(routes_isp1) == 0: 
+        ipr.route("add", dst="0.0.0.0/0", gateway=isp1_gw_ip, table=216) 
+    elif routes_isp1[0].get_attr('RTA_GATEWAY') != isp1_gw_ip:
+        ipr.route("del", dst="0.0.0.0/0", table=216)
+        ipr.route("add", dst="0.0.0.0/0", gateway=isp2_gw_ip, table=216) 
+
+    if len(routes_isp2) == 0:
+        ipr.route("add", dst="0.0.0.0/0", gateway=isp2_gw_ip, table=217)
+    elif routes_isp2[0].get_attr('RTA_GATEWAY') != isp2_gw_ip:
+        ipr.route("del", dst="0.0.0.0/0", table=217)
+        ipr.route("add", dst="0.0.0.0/0", gateway=isp2_gw_ip, table=217) 
+
 #debug    print routes
     if len(routes) != 0:
         if routes[0].get_attr('RTA_MULTIPATH') != None:
@@ -35,11 +90,11 @@ def check_def_routes():
         print "\nNo default routes found in routing table."
         return 2, None
 
-def check_isp_links(isp_if, f_testip1, f_testip2, f_testip3, f_isp_gw):
+def check_isp_links(isp_addr, f_testip1, f_testip2, f_testip3, f_isp_gw):
     gw_alive = False
     internet_state = False
     health_counter = 0
-    fping = Popen(['fping', '-I', isp_if, f_isp_gw, f_testip1, f_testip2, f_testip3], stdout=PIPE).communicate()
+    fping = Popen(['fping', '-S', isp_addr, f_isp_gw, f_testip1, f_testip2, f_testip3], stdout=PIPE).communicate()
     result = [str(i) for i in fping[0].strip().split() if i != 'is']
     if result[1] == 'alive':
        gw_alive = True 
@@ -54,17 +109,25 @@ def check_isp_links(isp_if, f_testip1, f_testip2, f_testip3, f_isp_gw):
         internet_state = True
     else:
         internet_state = False
-    print health_counter
+    
+#    print "This link has %d hosts alive." % health_counter
     return gw_alive, internet_state
 
 while True:
-    def_route = check_def_routes()
-    
-    isp1Link_state = check_isp_links(isp1_if, testip1, testip2, testip3, isp1_gw_ip)
-    isp2Link_state = check_isp_links(isp2_if, testip1, testip2, testip3, isp2_gw_ip)
-    print isp1Link_state
-    print isp2Link_state
+    if_state = check_if_state(isp1_if, isp2_if)
+    print if_state
 
+    def_route = check_def_routes()
+#debug    print def_route
+    if if_state[0] == False: 
+        isp1Link_state = check_isp_links(isp1_addr, testip1, testip2, testip3, isp1_gw_ip)
+    else:
+        isp1Link_state = [False, False]
+
+    if if_state[1] == False:
+        isp2Link_state = check_isp_links(isp2_addr, testip1, testip2, testip3, isp2_gw_ip)
+        print isp2Link_state
+    
     if isp1Link_state[0] and isp2Link_state[0] and def_route[0] == 2:
         print "Trying to install multipath default route."
         ipr.route("add", dst="0.0.0.0/0", multipath=[{"gateway": isp1_gw_ip, "hops": 0},{"gateway": isp2_gw_ip, "hops": 0}]),
@@ -101,36 +164,3 @@ while True:
             print "Removing default route."
             ipr.route("del", dst="0.0.0.0/0") 
     time.sleep(10)
-
-'''
-links = ipr.get_links()
-addr = ipr.get_addr()
-
-def get_int_state(isp_if):
-        for i in xrange(len(links)):
-                if links[i].get_attr('IFLA_IFNAME') == isp_if:
-                        return i, str(links[i].get_attrs('IFLA_OPERSTATE')).strip('\'[]\''), str(addr[i].get_attrs('IFA_ADDRESS')).strip('\'[]\'') #, links[i].get_attrs('IFLA_
-
-if ipr.link_lookup(ifname = isp1_if):
-	isp1_ifState = get_int_state(isp1_if)
-	print "The index of %s interface in list is %d, oper state is %s and ip-address is %s" % (isp1_if,isp1_ifState[0],isp1_ifState[1],isp1_ifState[2])
-else:
-	print "Interface %s not found in the system. Please check your configuration." % isp1_if
-
-if ipr.link_lookup(ifname = isp2_if):
-	isp2_ifState = get_int_state(isp2_if)
-	print "The index of %s interface in list is %d, oper state is %s and ip-address is %s\n" % (isp2_if,isp2_ifState[0],isp2_ifState[1],isp2_ifState[2])
-else:
-        print "Interface %s not found in the system. Please check your configuration." % isp2_if
-
-if isp1_ifState[1] == 'UP' and isp2_ifState[1] == 'UP' and isp2_ifState[2] == isp2_gw_ip:
-	print "Both interfaces are up and working fine. Insert multipath default route in routing table main."
-'''
-
-
-
-
-
-
-
-
