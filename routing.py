9d4c77e8c39fa39406329103e76b37580597a64d
Maksim Chumakov <mg.chumakov@gmail.com>
2016-10-13 11:17:24 +0300

    some changes

--- routing.py
+++ routing.py
@@ -60,18 +60,28 @@ def check_if_state(f_isp1_if, f_isp2_if):
         
 #Function to check which default route installed in routing table.
 def check_def_routes():
+    if_state = check_if_state(isp1_if, isp2_if)
     routes = ipr.get_routes(dst_len=0, table=254) #Obtain all default routes in routing table main. But we need to check return value to handle situation when there is no any default route installed.
-    routes_isp1 = ipr.get_routes(dst_len=0, table=216) #Obtain route information from table 216 - ISP1
+    try:
+        routes_isp1 = ipr.get_routes(dst_len=0, table=216) #Obtain route information from table 216 - ISP1
+        print len(routes_isp1)
+    except pyroute2.netlink.exceptions.NetlinkError:
+        print "Netlink exception!!!"
+        routes_isp1 = [] 
+    if len(routes_isp1) != '0': 
+        isp1_cur_rt_gw = routes_isp1[0].get_attr('RTA_GATEWAY') 
+    #else jj:
+        
     routes_isp2 = ipr.get_routes(dst_len=0, table=217) #obtain route information from table 217 - ISP2
 #then we check if there corresponding deafult routes installes in each of the ISP tables
 #if there is no default route installes we insert it, if there is incorrect one we change it to correct one
-    if len(routes_isp1) == 0: 
+    if len(routes_isp1) == '0' and if_state[0] != True: 
         ipr.route("add", dst="0.0.0.0/0", gateway=isp1_gw_ip, table=216) 
-    elif routes_isp1[0].get_attr('RTA_GATEWAY') != isp1_gw_ip:
+    elif isp1_cur_rt_gw != isp1_gw_ip and if_state[0] != True:
         ipr.route("del", dst="0.0.0.0/0", table=216)
         ipr.route("add", dst="0.0.0.0/0", gateway=isp2_gw_ip, table=216) 
 
-    if len(routes_isp2) == 0:
+    if len(routes_isp2) == 0 and if_state[1] != True:
         ipr.route("add", dst="0.0.0.0/0", gateway=isp2_gw_ip, table=217)
     elif routes_isp2[0].get_attr('RTA_GATEWAY') != isp2_gw_ip:
         ipr.route("del", dst="0.0.0.0/0", table=217)
